32db8584878cd27fb4333d65df54fb3e2bf7dda8
Кобес Тамерлан <kobestamerlan@MacBook-Air-Kobes.local>
2026-05-22 19:09:50 +0500

    fix: update main.ts for Render compatibility

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Используем порт 0 (случайный свободный) чтобы избежать ошибки EADDRINUSE при тестировании
-  await app.listen(process.env.PORT ?? 0);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
